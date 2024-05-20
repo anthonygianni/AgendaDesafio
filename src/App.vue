e4247baac9f54f1769c39c8d9b3195137c6f3d53
anthonygianni <anthonygianni722@outlook.com>
2024-05-20 16:20:29 -0300

    alterações do front baseado nos retornos async do back

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Apresentação</router-link> |
+    <router-link to="/requirements">Requisitos</router-link> |
+    <router-link to="/schedule">Agenda</router-link>
   </nav>
   <router-view/>
 </template>
